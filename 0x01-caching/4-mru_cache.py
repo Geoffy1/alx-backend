f5b08e6f5ff3e6a13193f984f0fe0329d8bcd492
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-06-04 15:19:07 +0300

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,39 +1,39 @@
 #!/usr/bin/env python3
+"""Most Recently Used cachings
 """
-MRUCache module
-"""
+from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ MRUCache class
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with an MRU
+    removal mechanism when the limits is reached.
     """
-
     def __init__(self):
-        """ Initialize
+        """Initializes the caches.
         """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """Adds an item in the caches.
         """
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                # Move the key to the most recently used position (MRU algorithm)
-                self.cache_data.pop(key)
-            elif len(self.cache_data) >= self.MAX_ITEMS:
-                # Discard the most recently used item (MRU algorithm)
-                mru_key = next(reversed(self.cache_data))
-                del self.cache_data[mru_key]
-                print("DISCARD: {}".format(mru_key))
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key
+        """Retrieve an item by key.
         """
-        if key is None or key not in self.cache_data:
-            return None
-        # Move the key to the most recently used position (MRU algorithm)
-        item = self.cache_data.pop(key)
-        self.cache_data[key] = item
-        return item
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
